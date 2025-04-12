6b6529cccc6a0eb263934ed177f5c052f7d4a54f
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2025-04-12 20:00:59 +0500

    update

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,16 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+	getAuth,
+	EmailAuthProvider,
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import * as firebaseui from "https://www.gstatic.com/firebasejs/ui/6.0.2/firebase-ui.js";
+import "https://www.gstatic.com/firebasejs/ui/6.0.2/firebase-ui.css";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: "AIzaSyDr1MLlIMbJyi36DWBxMlL_z8BN760TOHE",
 	authDomain: "pomodoro-app-775fc.firebaseapp.com",
 	projectId: "pomodoro-app-775fc",
-	storageBucket: "pomodoro-app-775fc.firebasestorage.app",
+	storageBucket: "pomodoro-app-775fc.appspot.com",
 	messagingSenderId: "418755239953",
 	appId: "1:418755239953:web:8fb1fd26af5cea43dd11b0",
 	measurementId: "G-R3PC6T083R",
@@ -19,32 +20,24 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 // Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+const ui = new firebaseui.auth.AuthUI(auth);
 
-var uiConfig = {
+const uiConfig = {
 	callbacks: {
 		signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-			// User successfully signed in.
-			// Return type determines whether we continue the redirect automatically
-			// or whether we leave that to developer to handle.
 			return true;
 		},
 		uiShown: function () {
-			// The widget is rendered.
-			// Hide the loader.
 			document.getElementById("loader").style.display = "none";
 		},
 	},
-	// Will use popup for IDP Providers sign-in flow instead of the default, redirect.
 	signInFlow: "popup",
 	signInSuccessUrl: "https://nash268.github.io/pomodoro/",
-	signInOptions: [
-		// Leave the lines as is for the providers you want to offer your users.
-		firebase.auth.EmailAuthProvider.PROVIDER_ID,
-	],
+	signInOptions: [EmailAuthProvider.PROVIDER_ID],
 };
 
-// The start method will wait until the DOM is loaded.
+// Start FirebaseUI
 ui.start("#firebaseui-auth-container", uiConfig);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pomodoro-cache-88821";
+const CACHE_NAME = "pomodoro-cache-738792";
 const APP_PREFIX = "pomodoro_";
 const ASSETS_TO_CACHE = [
 	"/pomodoro/",
